1102a4e44bdd506d1dbc7b820dcc82be47d3a497
Abhoir1 <aditibhoir7276@gmail.com>
2023-03-11 17:31:43 -0500

    took goal and starting point from user

--- code.py
+++ code.py
@@ -1,6 +1,26 @@
 import numpy as np
+from queue import PriorityQueue
+
+start_x_coordinate = input("Enter x coordinate of starting point \n")
+start_y_coordinate = input("Enter y coordinate of starting point \n")
+
+starting_point = (int(start_x_coordinate), int(start_y_coordinate))
+
+goal_x_coordinate = input("Enter x coordinate of goal point \n")
+goal_y_coordinate = input("Enter y coordinate of goal point \n")
+
+goal_point = (int(goal_x_coordinate), int(goal_y_coordinate))
 
 # Defining the obstacles
+
+def outer_boundary(point_coordinates):
+    x = point_coordinates[0]
+    y = point_coordinates[1]
+
+    if (x <= 0) and (x >= 300) and (y <=0) and (y >= 250):
+        return True
+    else:
+        return False
 
 def rectangular_up_obstacle(point_coordinates):
     x = point_coordinates[0]
@@ -37,19 +57,80 @@ def triangular_obstacle(point_coordinates):
         return True
     else:
         False
-
-def outer_boundary(point_coordinates):
-    x = point_coordinates[0]
-    y = point_coordinates[1]
-
-    if (x <= 0) and (x >= 300) and (y <=0) and (y >= 250):
-        return True
-    else:
-        return False
     
 def all_obstacles(point_coordinates):
     if (rectangular_down_obstacle(point_coordinates) or rectangular_up_obstacle(point_coordinates) or hexagonal_obstacle(point_coordinates) or triangular_obstacle(point_coordinates)) == True:
         return True
     else:
         return False
+
+while all_obstacles(starting_point):
+    print("The given starting point lies in the obstacle space, enter new starting point \n")
+    start_x_coordinate = input("Enter x coordinate of starting point \n")
+    start_y_coordinate = input("Enter y coordinate of starting point \n")
+
+while all_obstacles(goal_point):
+    print("The given goal point lies in the obstacle space, enter new goal point \n")
+    goal_x_coordinate = input("Enter x coordinate of goal point \n")
+    goal_y_coordinate = input("Enter y coordinate of goal point \n")
+
+x_coordinate_visited = []
+y_coordinate_visited = []
+
+start = starting_point
+goal = goal_point
+
+c2c = 0
+parent_node = None
+
+source_node = (c2c, (parent_node), (start))
+
+list_open = PriorityQueue()
+list_closed = {}
+list_open.put(source_node)
+
+
+def move_left(source_node, all_obstacles, list_closed, list_open):
+    c2c_left = source_node[0] + 1
+    parent_node_left = source_node[2]
+
+    x_coordinate_new = source_node[2][0] - 1
+    y_coordinate_new = source_node[2][1]
+
+    node_new = (x_coordinate_new, y_coordinate_new)
+
+    if all_obstacles(node_new) == False:
+        if node_new not in list_closed:
+            node_new_left = (c2c_left, parent_node_left, node_new)
+            list_open.put(node_new_left)
+    return list_open
+
+def move_right(source_node, all_obstacles, list_closed, list_open):
+    c2c_right = source_node[0] + 1
+    parent_node_right = source_node[2]
+
+    x_coordinate_new = source_node[2][0] + 1
+    y_coordinate_new = source_node[2][1]
+
+    node_new = (x_coordinate_new, y_coordinate_new)
+
+    if all_obstacles(node_new) == False:
+        if node_new not in list_closed:
+            node_new_right = (c2c_right, parent_node_right, node_new)
+            list_open.put(node_new_right)
+    return list_open
+
+def move_up(source_node, all_obstacles, list_closed, list_open):
+    c2c_up = source_node[0] + 1
+    parent_node_up = source_node[2]
+
+    x_coordinate_new = source_node[2][0] 
+    y_coordinate_new = source_node[2][1] + 1
+
+    node_new = (x_coordinate_new, y_coordinate_new)
+
+    if all_obstacles(node_new) == False:
+        if node_new not in list_closed:
+            node_new_up = (c2c_up, parent_node_up, node_new)
+            list_open.put(node_new_up)
     
