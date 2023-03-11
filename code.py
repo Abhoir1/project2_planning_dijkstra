33c881a8e09bd07c9f02365032a171a54e5cc98b
Abhoir1 <aditibhoir7276@gmail.com>
2023-03-11 15:18:02 -0500

    added outer boundary

--- code.py
+++ code.py
@@ -15,7 +15,7 @@ def rectangular_down_obstacle(point_coordinates):
     x = point_coordinates[0]
     y = point_coordinates[1]
 
-    if x >= 100 and x <= 150 and y>=0 and y <= 100:
+    if (x >= 100) and (x <= 150) and (y>=0) and (y <= 100):
         return True
     else:
         return False
@@ -24,7 +24,7 @@ def hexagonal_obstacle(point_coordinates):
     x = point_coordinates[0]
     y = point_coordinates[1]
 
-    if x >= 64.95 and x <= 364.95 and (((y - 162.5) - ((162.5 - 200)/ (64.95 - 300))* (x - 64.95)) >= 0) and (((y - 200) - ((200 - 162.5)/(300 - 364.95))* (x - 300)) <= 0) and (((y - 87.5) - ((87.5 - 50)/ (364.95 - 300))* (x - 364.95)) <= 0) and (((y - 87.5) - ((87.5 - 50)/ (364.95 - 300))* (x - 364.95)) >= 0):
+    if x >= 64.95 and x <= 364.95 and (((y - 162.5) - ((162.5 - 200)/ (64.95 - 300))* (x - 64.95)) <= 0) and (((y - 200) - ((200 - 162.5)/(300 - 364.95))* (x - 300)) <= 0) and (((y - 87.5) - ((87.5 - 50)/ (364.95 - 300))* (x - 364.95)) >= 0) and (((y - 87.5) - ((87.5 - 50)/ (364.95 - 300))* (x - 364.95)) >= 0):
         return True
     else:
         return False
@@ -33,8 +33,16 @@ def triangular_obstacle(point_coordinates):
     x = point_coordinates[0]
     y = point_coordinates[1]
 
-    if x>= 460 and (((y - 225) - ((225 - 125)/ (460 - 510))*(x - 460)) <= 0) and (((y - 125) - ((125 - 25)/ (510 - 460))*(x - 510)) <= 0):
+    if x>= 460 and (((y - 225) - ((225 - 125)/ (460 - 510))*(x - 460)) <= 0) and (((y - 125) - ((125 - 25)/ (510 - 460))*(x - 510)) >= 0):
         return True
     else:
         False
 
+def outer_boundary(point_coordinates):
+    x = point_coordinates[0]
+    y = point_coordinates[1]
+
+    if (x>= 0) and (x <= 300) and (y >=0) and (y <= 250):
+        return True
+    else:
+        return False
